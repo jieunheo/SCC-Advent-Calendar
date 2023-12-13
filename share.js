4f43dc9362a0cf619484fcb6ccc68e3d84d6abd2
jieunheo <45378398+jieunheo@users.noreply.github.com>
2023-12-13 10:04:46 +0900

    오류 수정

--- share.js
+++ share.js
@@ -21,3 +21,19 @@ document.getElementById('shareButton').addEventListener('click', async () => {
     console.error('Share failed:', err.message);
   }
 });
+
+// 배경음 실행 관련
+document.addEventListener('DOMContentLoaded', function() {
+  const soundElement = document.querySelector('.sound');
+  const stopElement = document.querySelector('.stop');
+  const bgm = document.querySelector('.bgm');
+
+  soundElement.addEventListener('click', function() {
+    bgm.play();
+  });
+
+  stopElement.addEventListener('click', function() {
+    bgm.pause();
+    bgm.currentTime = 0;
+  });
+});
